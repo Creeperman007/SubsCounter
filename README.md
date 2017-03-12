6fa7ec56ee105945a5c3c734e9127bb219ec29ed
Creeperman007 <foltinmatej@gmail.com>
2017-03-12 17:21:56 +0100

    v0.2

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 #YouTube Live Subscriber Count
 ##Info
 This app can be used for showing the live count of subscribers<br>
-In this state, you can only use the YouTube channel ID but don't worry, I'm working on the *search* function.<br>
-~~App also includes the *embed* version. It have transparent background, so you can use it in livestreaming in OBS etc.~~
+You can search strings like *pewdiepie* and you can also use the channel ID, e.g. *UC-lHJZR3Gqxm24_Vd_AJ5Yw*
+I promise, that embed version will be in v0.3
 ##How To
-As I said, it can accept only Channel ID. If you don't know, how to get it, don't worry. Until I'll publish the updated version, you can use [this app](https://johnnythetank.github.io/youtube-channel-name-converter/) to get the ID.
+When you'll write the search string, it'll use Google API to search for the matching channel.
+##Embed
+If you want to add the embed version in your stream, but you don't want to wait, you can use this URL:<br>
+`https://creeperman007.github.io/projects/subscounter/embed/?uid=channellName&color=000000`<br>
+You have to edit following parameters:
+* `uid` is the search string (example: `uid=pewdiepie`)
+* `color` is color of the text. You have to use *hex code* (example: `color=000000` - the text will be black)<br>
+Example url: [https://creeperman007.github.io/projects/subscounter/embed/?uid=pewdiepie&color=000000](https://creeperman007.github.io/projects/subscounter/embed/?uid=pewdiepie&color=000000)
 ##Download
+[Version 0.2](https://github.com/Creeperman007/subscounter/releases/tag/v0.2)
 [Version 0.1](https://github.com/Creeperman007/subscounter/releases/tag/v0.1)
 <br><br>
 More info coming soon...
--- source/resources/app/src/script.main.js
+++ source/resources/app/src/script.main.js
@@ -20,29 +20,35 @@ if ($_GET['uid']) {
   xmlhttp1.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       myObj = JSON.parse(this.responseText);
-      document.getElementById("title").innerHTML = myObj.items[0].snippet.title;
-
+      if (myObj.pageInfo.totalResults == 0) {
+        document.getElementById("title").innerHTML = "No results. Please, try again.";
+      }
+      else {
+        document.getElementById("title").innerHTML = myObj.items[0].snippet.title;
+        var id = myObj.items[0].snippet.channelId;
+        //START SubsCount
+        document.getElementById("subs").innerHTML = "Loading...";
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            myObj = JSON.parse(this.responseText);
+            var n = myObj.items[0].statistics.subscriberCount;
+            n = parseInt(n);
+            document.getElementById("subs").innerHTML = n.toLocaleString('en-US');
+          }
+        };
+        function getApi() {
+          xmlhttp.open("GET", "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + id + "&key=AIzaSyCEm6mS_IlWaKokdUGk1R1ZVapxFsj64qM", true);
+          xmlhttp.send();
+        }
+        var r = setInterval(getApi, 1000);
+        //END SubsCount
+      }
     }
   };
-  xmlhttp1.open("GET", "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + $_GET['uid'] + "&key=AIzaSyCEm6mS_IlWaKokdUGk1R1ZVapxFsj64qM", true);
-  xmlhttp1.send();
-  //Showing Subscriber Count
-  document.getElementById("subs").innerHTML = "Loading...";
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      myObj = JSON.parse(this.responseText);
-      var n = myObj.items[0].statistics.subscriberCount;
-      n = parseInt(n);
-      document.getElementById("subs").innerHTML = n.toLocaleString('en-US');
-
-    }
-  };
-  function getApi() {
-    xmlhttp.open("GET", "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + $_GET['uid'] + "&key=AIzaSyCEm6mS_IlWaKokdUGk1R1ZVapxFsj64qM", true);
-    xmlhttp.send();
+  for (i = 0; i < 5; i++) {
+    xmlhttp1.open("GET", "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + $_GET['uid'] + "&key=AIzaSyCEm6mS_IlWaKokdUGk1R1ZVapxFsj64qM", true);
+    xmlhttp1.send();
   }
-  var r = setInterval(getApi, 1000);
-  document.getElementById("embed").innerHTML = "<a href=\"embed.html?uid=" + $_GET['uid'] + "&color=000000\" target=\"_blank\">embed</a>";
 }
 else {}
